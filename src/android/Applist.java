4e07110fe48bf82b58141557836379be2d32618e
Satria Prayudi <satryacode@gmail.com>
2017-10-25 13:39:41 +0700

    removed Drawable library due to never use it

--- src/android/Applist.java
+++ src/android/Applist.java
@@ -20,7 +20,7 @@ import java.io.ByteArrayOutputStream;
 import android.os.Environment;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.BitmapDrawable;  
+import android.graphics.drawable.BitmapDrawable;
 import android.content.Context;
 import android.graphics.PixelFormat;
 import java.util.List;
@@ -29,29 +29,6 @@ import java.util.HashSet;
 import java.util.Collections;
 
 public class Applist extends CordovaPlugin {
-    public static void drawableTofile(Drawable drawable,String path)
-    {
-
-            File file = new File(path);
-            Bitmap bitmap=((BitmapDrawable)drawable).getBitmap();
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100 /*ignored for PNG*/, bos);
-            byte[] bitmapdata = bos.toByteArray();
-
-
-            //write the bytes in file
-            FileOutputStream fos;
-            try {
-                fos = new FileOutputStream(file);
-                fos.write(bitmapdata);
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-
-    }
-
-
     public String getSDPath()
      {
             File SDdir=null;
@@ -67,32 +44,18 @@ public class Applist extends CordovaPlugin {
                     return null;
             }
     }
-    
+
     public String getDataPath() {
       Context context = this.cordova.getActivity().getApplicationContext();
       return context.getFilesDir().getPath();
     }
 
-
-    public static void makeRootDirectory(String filePath) {  
-        File file = null;  
-        try {  
-            file = new File(filePath);  
-            if (!file.exists()) {
-                file.mkdirs();  //make Directory
-            }  
-        } catch (Exception e) {  
-             e.printStackTrace();
-        }  
-    }
-
-
     @Override
-    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException 
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException
     {
         if(action.equals("getApps"))
-        { 
-            cordova.getThreadPool().execute(new Runnable() 
+        {
+            cordova.getThreadPool().execute(new Runnable()
             {
                 public void run()
                 {
@@ -101,12 +64,10 @@ public class Applist extends CordovaPlugin {
                         //get a list of installed apps.
                         PackageManager pm = cordova.getActivity().getPackageManager();
                         List<ApplicationInfo> packages = pm.getInstalledApplications(0);
-                        
+
                         JSONArray  app_list = new JSONArray();
                         int cnt =0;
-                        String path=getDataPath();
-                        makeRootDirectory(path+"/Applist/");
-                        for (ApplicationInfo packageInfo : packages) 
+                        for (ApplicationInfo packageInfo : packages)
                         {
                             if ((packageInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 1)
                             {
@@ -114,27 +75,15 @@ public class Applist extends CordovaPlugin {
                             }
                             else
                             {
-                                  JSONObject info = new JSONObject();  
+                                  JSONObject info = new JSONObject();
                                   info.put("id",packageInfo.packageName);
                                   info.put("name",packageInfo.loadLabel(pm));
-                                  String img_name =  "/Applist/"+ packageInfo.packageName +".png";
-                                  info.put("img",path+img_name);
-                                  //cheak exist  or not
-                                  File  cheakfile  = new File( path + img_name );
-                                  if(  !cheakfile.exists()  )
-                                  {
-                                      Drawable icon = pm.getApplicationIcon(packageInfo);
-                                      if(icon!=null)
-                                      {
-                                          drawableTofile(icon,  path+img_name);
-                                      }
-                                  }
                                   app_list.put(cnt++,info);
                             }
                         }
                         callbackContext.success( app_list );
-                     } 
-                     catch(Exception e) 
+                     }
+                     catch(Exception e)
                      {
                         System.err.println("Exception: " + e.getMessage());
                         callbackContext.error(e.getMessage());
@@ -144,8 +93,8 @@ public class Applist extends CordovaPlugin {
             return true;
         }
         if(action.equals("getAllApps"))
-        { 
-            cordova.getThreadPool().execute(new Runnable() 
+        {
+            cordova.getThreadPool().execute(new Runnable()
             {
                 public void run()
                 {
@@ -156,7 +105,7 @@ public class Applist extends CordovaPlugin {
                         Intent intent = new Intent(Intent.ACTION_MAIN, null);
                         intent.addCategory(Intent.CATEGORY_LAUNCHER);
                         List<ResolveInfo> resInfos = pm.queryIntentActivities(intent, 0);
-                        //using hashset so that there will be no duplicate packages, 
+                        //using hashset so that there will be no duplicate packages,
                         //if no duplicate packages then there will be no duplicate apps
                         HashSet<String> packageNames = new HashSet<String>(0);
                         List<ApplicationInfo> appInfos = new ArrayList<ApplicationInfo>(0);
@@ -178,37 +127,23 @@ public class Applist extends CordovaPlugin {
 
                         //to sort the list of apps by their names
                         Collections.sort(appInfos, new ApplicationInfo.DisplayNameComparator(pm));
-                    
+
                         //get a list of installed apps.
                         //PackageManager pm = cordova.getActivity().getPackageManager();
                         //List<ApplicationInfo> packages = pm.getInstalledApplications(0);
-                        
+
                         JSONArray  app_list = new JSONArray();
                         int cnt =0;
-                        String path=getDataPath();
-                        makeRootDirectory(path+"/Applist/");
-                        for (ApplicationInfo packageInfo : appInfos) 
+                        for (ApplicationInfo packageInfo : appInfos)
                         {
-                            JSONObject info = new JSONObject();  
+                            JSONObject info = new JSONObject();
                             info.put("id",packageInfo.packageName);
                             info.put("name",packageInfo.loadLabel(pm));
-                            String img_name =  "/Applist/"+ packageInfo.packageName +".png";
-                            info.put("img",path+img_name);
-                            //cheak exist  or not
-                            File  cheakfile  = new File( path + img_name );
-                            if(  !cheakfile.exists()  )
-                            {
-                                Drawable icon = pm.getApplicationIcon(packageInfo);
-                                if(icon!=null)
-                                {
-                                    drawableTofile(icon,  path+img_name);
-                                }
-                            }
                             app_list.put(cnt++,info);
                         }
                         callbackContext.success( app_list );
-                     } 
-                     catch(Exception e) 
+                     }
+                     catch(Exception e)
                      {
                         System.err.println("Exception: " + e.getMessage());
                         callbackContext.error(e.getMessage());
@@ -220,5 +155,5 @@ public class Applist extends CordovaPlugin {
         //
         callbackContext.error("Invalid action");
         return false;
-    }     
+    }
 }
